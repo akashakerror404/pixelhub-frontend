22fceadba6f7819bd681094730462781187f35fb
akash <akashak1602@gmail.com>
2023-10-30 14:09:33 +0530

    host 0.2

--- pixelhub/src/Componets/User/Navbar/Customenavbar.jsx
+++ pixelhub/src/Componets/User/Navbar/Customenavbar.jsx
@@ -57,7 +57,7 @@ function Customenavbar() {
               <div className='hidden md:flex'>
                 <ul className='flex gap-9'>
                     <li className='group relative px-3 py-2 text-sm font-medium' onClick={()=>navigate('/')}>
-                    HOME
+                    HOME 
                     <div className='absolute inset-x-0 bottom-0 h-1 bg-[#00A9B7] transform scale-x-0 origin-left transition-transform group-hover:scale-x-100'></div>
                     </li>
                     <li className='group relative px-3 py-2 text-sm font-medium'onClick={()=>navigate('/course')}>
